eb4a743154e1aa64be66c142f11459f373da8a7d
luvanidixit101 <luvanidixit101@gmail.com>
2025-07-30 15:51:54 +0000

    hello

--- hello.py
+++ hello.py
@@ -1,3 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, world!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3952)
 print("hello wold")
 
 # Example usage
