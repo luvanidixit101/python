662fd5c97ef5d96296cbbd7ef4142bcb2597d6de
luvanidixit101 <luvanidixit101@gmail.com>
2025-07-30 15:43:39 +0000

    hello

--- hello.py
+++ hello.py
@@ -1,2 +1,8 @@
 print("hello wold")
 
+# Example usage
+text = "I am learning python programming."
+if "python" in text:
+    print("The word 'python' is in the string!")
+else:
+    print("The word 'python' is not in the string.")
